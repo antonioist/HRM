1715beb4a6ea9e4bf0045d75eb5556628df7c1d0
Antonio Sanchez <antonio.sanchez.trespalacios@gmail.com>
2020-05-27 13:40:32 +0900

    basic validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  has_many :bookings
+  has_many :jobs
+  has_many :informations
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable
 end
