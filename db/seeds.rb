0cc4e29af99ecb070422fab1ffbfda0749745e52
Antonio Sanchez <antonio.sanchez.trespalacios@gmail.com>
2020-06-02 14:26:18 +0900

    3 different kind of users ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Obliterating everthing"
-
+User.destroy_all
 puts "end"
 
 
 puts "Creating Users"
 
-puts "end"
+antonio = User.create!(
+  first_name: "Antonio",
+  last_name: "Sanchez",
+  email: "antonio@lewagon.com",
+  password: "123123",
+  superadmin_role: true
+)
+
+linchien = User.create!(
+  first_name: "Linchien",
+  last_name: "Huang",
+  email: "linchien@lewagon.com",
+  password: "123123",
+  supervisor_role: true
+)
+
+isidoro = User.create!(
+  first_name: "Isidoro",
+  last_name: "Trespalacios",
+  email: "isidoro@lewagon.com",
+  password: "123123"
+)
+
+puts "Users ready"
 
 puts "Creating Jobs"
 
